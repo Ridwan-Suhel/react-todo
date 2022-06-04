fcb9568829a9a04ea9f6467be6d0d41a6fb54bf0
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-06-05 01:40:58 +0600

    todo completed button added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,35 @@
-import { CheckCircleIcon, TrashIcon } from "@heroicons/react/solid";
+import { CheckCircleIcon, TrashIcon, StopIcon } from "@heroicons/react/solid";
 import React from "react";
 
 const Todo = ({ setTodos, todos, todo }) => {
-  const { id, text } = todo;
+  const { id, text, completed } = todo;
   const handleTodoDelete = () => {
     setTodos(todos.filter((el) => el.id !== id));
   };
+  const handleTodoComplete = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
+  console.log(completed);
   return (
     <div className="todo">
-      <li>{text}</li>
-      <button>
-        <CheckCircleIcon className="icon green" />
+      <li className={`${completed ? "completed" : ""}`}>{text}</li>
+      <button onClick={handleTodoComplete}>
+        {completed === false ? (
+          <CheckCircleIcon className="icon green" />
+        ) : (
+          <StopIcon className="icon green"></StopIcon>
+        )}
       </button>
       <button onClick={handleTodoDelete}>
         <TrashIcon className="icon red" />
